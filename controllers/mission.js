10154f37477317093a7627129879187c5a1052f2
3MPTYB0Y <TheOfficialRedsifter@gmail.com>
2023-03-02 10:12:34 +0100

    Get routes for mission done

--- controllers/mission.js
+++ controllers/mission.js
@@ -2,37 +2,60 @@ const { Sequelize,sequelize, Mission, Plante, Profile } = require('../models');
 
 // create a mission
 exports.createMission = async (req, res) => {
-    
-    let plante = await Plante.findOne({where:{id : req.body.planteId}});
-    if(!plante) {
-      return res.status(404).json("Plante non trouvée");
-    }
-    
-    let profile = await Profile.findOne({where:{id : plante.ProfileId}});
-    if(profile.userUid !== res.locals.userId && !res.locals.isAdmin) {
-      return res.status(403).json("Vous n'êtes pas autorisé à accéder à cette plante");
-    }
+    try{
+      let plante = await Plante.findOne({where:{id : req.body.planteId}});
+      if(!plante) {
+        return res.status(404).json("Plante non trouvée");
+      }
+      
+      let profile = await Profile.findOne({where:{id : plante.ProfileId}});
+      if(profile.userUid !== res.locals.userId && !res.locals.isAdmin) {
+        return res.status(403).json("Vous n'êtes pas autorisé à accéder à cette plante");
+      }
 
-    let missionCreated = {
-      PlanteId: req.body.planteId,
-      description: req.body.description,
-      dateDebut: req.body.dateDebut,
-      dateFin: req.body.dateFin,
-      accomplie: false
-    };
-    Mission.create(missionCreated).then(mission => {
+      let missionCreated = {
+        PlanteId: req.body.planteId,
+        description: req.body.description,
+        dateDebut: req.body.dateDebut,
+        dateFin: req.body.dateFin,
+        accomplie: false
+      };
+      let mission = await Mission.create(missionCreated);
       res.status(201).json(mission);
-
-    }).catch(err => {
-      res.status(500).json({ message: 'Server error' });
-    });
+    } catch(err){
+      res.status(500).json({ error : err });
+    }
 };
 
 // get all missions
 exports.getAllMissions = async (req, res) => {
   try {
-    const missions = await Mission.findAll();
-    res.json(missions);
+    if(req.params.get("view") == "admin"){
+      if(!res.locals.isAdmin){
+        res.status(403).json({ message: 'Vous ne pouvez pas accéder à la vue admin mission' });
+      }
+      else{
+        let missions = await Mission.findAll();
+        res.json(missions)
+      }
+    }
+    else if(req.params.get("view") == "caretaker") {
+      let profile = await Profile.findOne({where: {userUid: res.locals.userId}});
+      let missions = await Mission.findAll({where: {ProfileId: profile.id}});
+      res.json(missions);
+    }
+    else if(req.params.get("view") == "owner"){
+      let profile = await Profile.findOne({where: {userUid: res.locals.userId}});
+      let plantes = await Plante.findAll({where: {ProfileId: profile.id}});
+      let missions = [];
+      plantes.forEach(plante => {
+        Mission.findAll({where: {PlanteId: plante.id}}).then(mission => {
+          missions.push(mission);
+        })
+      });
+      res.json(missions);
+    }
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
@@ -45,13 +68,18 @@ exports.getMissionById = async (req, res) => {
   try {
     const mission = await Mission.findByPk(id);
     if (!mission) {
-      res.status(404).json({ message: 'Mission not found' });
-      return;
+      return res.status(404).json({ message: 'Mission non trouvée' });
+    }
+
+    let plante = await Plante.findByPk(mission.PlanteId);
+    let profileMission = await Profile.findByPk(mission.ProfileId);
+    let profilePlante = await Profile.findByPk(plante.ProfileId);
+    if(profilePlante.userUid !== res.locals.userId && !res.locals.isAdmin && profileMission.userUid !== res.locals.userId){
+      res.status(403).json({ message: 'Vous ne pouvez pas accéder à cette mission' });
     }
     res.json(mission);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+  } catch (err) {
+    res.status(500).json({ error: err });
   }
 };
 
@@ -59,9 +87,16 @@ exports.getMissionById = async (req, res) => {
 exports.editMission = async (req, res) => {
   const { id } = req.params;
   try {
+
+    let plante = await Plante.findOne({where:{id : req.body.planteId}});
+    
+    if(!plante) {
+      return res.status(404).json("Plante non trouvée");
+    }
+
     const mission = await Mission.findByPk(id);
     if (!mission) {
-      res.status(404).json({ message: 'Mission not found' });
+      res.status(404).json({ message: 'Mission non trouvée' });
       return;
     }
     let missionUpdated = {
@@ -72,13 +107,16 @@ exports.editMission = async (req, res) => {
       dateFin: req.body.dateFin,
       accomplie: req.body.accomplie
     };
+    if(missionUpdated.ProfileId == plante.ProfileId){
+      res.status(403).json({ message: 'Mission et Plante le peuvent pas avoir le même ProfileId' });
+    }
     await mission.update({...missionUpdated },{where:{id : id}});
 
 
     res.json(mission);
-  } catch (error) {
+  } catch (err) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ error : err });
   }
 };
 
@@ -86,15 +124,19 @@ exports.editMission = async (req, res) => {
 exports.deleteMission = async (req, res) => {
   const { id } = req.params;
   try {
-    const mission = await Mission.findByPk(id);
+    let mission = await Mission.findByPk(id);
     if (!mission) {
-      res.status(404).json({ message: 'Mission not found' });
-      return;
+      return res.status(404).json({ message: 'Mission non trouvée' });
+    }
+    let plante = await Plante.findByPk(mission.PlanteId);
+    let profile = await Profile.findByPk(plante.ProfileId);
+    if(profile.userUid !== res.locals.userId && !res.locals.isAdmin){
+      res.status(403).json({ message: 'Vous ne pouvez pas accéder à cette mission' });
     }
     await mission.destroy();
     res.status(204).json();
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    console.error(err);
+    res.status(500).json({ error : err });
   }
 };
