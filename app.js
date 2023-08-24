a4c85f0701de78cdef507c4099fd1766ec156c83
3MPTYB0Y <TheOfficialRedsifter@gmail.com>
2023-08-24 11:44:48 +0200

    Edited app.js

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ app.use(cors({
   credentials:true
 }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, multipart/form-data, JSON');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.setHeader('Cross-Origin-Embedder-Policy', '*');
