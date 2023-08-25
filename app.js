23c440168993209ca75f895ec68e91be54a286d3
3MPTYB0Y <TheOfficialRedsifter@gmail.com>
2023-08-25 14:39:28 +0200

    Edited ap.js

--- app.js
+++ app.js
@@ -41,12 +41,10 @@ app.use(cookieParser());
 
 app.use(helmet());
 
-app.use(cors({
-  origin:['https://warp-front.onrender.com','https://warp-api-2.onrender.com'],
-  credentials:true
-}));
+app.use(cors());
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://warp-api-2.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://warp-front.onrender.com');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, multipart/form-data, JSON');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.setHeader('Cross-Origin-Embedder-Policy', '*');
