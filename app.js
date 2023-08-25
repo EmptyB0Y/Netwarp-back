78c4041667e10633137c80873ea8fa38a73c39a2
3MPTYB0Y <TheOfficialRedsifter@gmail.com>
2023-08-25 15:01:46 +0200

    Added allow origin header

--- app.js
+++ app.js
@@ -45,12 +45,15 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://warp-front.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', '127.0.0.1');
+
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, multipart/form-data, JSON');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.setHeader('Cross-Origin-Embedder-Policy', '*');
   res.setHeader('Cross-Origin-Resource-Policy', '*');
   next();
 });
+
 app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/profiles', profileRouter);
